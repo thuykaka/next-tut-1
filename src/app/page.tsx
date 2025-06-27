d2023a636401f6573ebe84a790107ccc541f0db7
ThuyLe <thuylv.work@gmail.com>
2025-06-27 13:13:09 +0700

    chapter3: better-auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import { Button } from '@/components/ui/button';
+import { LoginForm } from '@/components/login-form';
 
 export default function Home() {
-  return <Button>Click me</Button>;
+  return (
+    <div className='bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10'>
+      <div className='w-full max-w-sm md:max-w-4xl'>
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
